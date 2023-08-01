5f13781c93176d684634f64ac578615ff0bb914d
faith <faith@marascientific.org>
2023-08-01 09:00:52 +0300

    introduction to pug

---
+++ app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'pug') //set any values globally to the express app
+app.set('views','views') //location of templates
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(5000);
---
+++ routes/shop.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin')
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  console.log(adminData.products)
+  res.render('shop')
+
+});
+
+module.exports = router;
