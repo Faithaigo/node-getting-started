d3b8257469ef8b8298a491072f16e3856f2c3052
faith <faith@marascientific.org>
2024-01-05 18:21:48 +0300

    Add edit product functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,12 +2,10 @@ const Product = require('../models/product')
 
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
-      pageTitle: 'Add Product',
-      path: '/admin/add-product',
-      formsCSS: true,
-      productCSS: true,
-      activeAddProduct: true
+    res.render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing:false
     });
   }
 
@@ -17,10 +15,44 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl
     const description = req.body.description
     const price = req.body.price
-    const product = new Product(title, imageUrl, description, price)
+    const product = new Product(null,title, imageUrl, description, price)
     product.save()
+
+    //send response
     res.redirect('/');
   }
+
+exports.getEditProduct = (req, res, next) => {
+    //add optional query parameters, can be used for example keeping a filter that a user set on the page
+    const editMode = req.query.edit;
+    if (!editMode){
+        return res.redirect('/')
+    }
+    const prodId = req.params.productId
+    Product.findById(prodId, product=>{
+        if (!product){
+            return res.redirect('/')
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit  Product',
+            path: '/admin/edit-product',
+            editing:editMode,
+            product
+        });
+    })
+
+}
+
+exports.postEditProduct = (req, res, next) =>{
+    const prodId = req.body.productId
+    const updated_title = req.body.title
+    const updated_imageUrl = req.body.imageUrl
+    const updated_description = req.body.description
+    const updated_price = req.body.price
+    const updatedProduct = new Product(prodId,updated_title, updated_imageUrl, updated_description, updated_price)
+    updatedProduct.save()
+    res.redirect('/')
+}
 
 exports.getAdminProducts = (req, res, next) => {
     Product.fetchAll(products=>{
--- routes/admin.js
+++ routes/admin.js
@@ -15,5 +15,7 @@ router.get('/add-product', adminController.getAddProduct); //refrence to the fun
 router.post('/add-product', adminController.postAddProduct);
 
 router.get('/products', adminController.getAdminProducts);
+router.get('/edit-product/:productId', adminController.getEditProduct);
+router.post('/edit-product', adminController.postEditProduct);
 
 module.exports = router;
