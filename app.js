8e4aac7e3423b633384c766a5298bed73de885ed
faith <faith@marascientific.org>
2024-02-07 14:47:40 +0300

    Store a reference to the user collection in the products collection

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 
 const {mongoConnect} = require('./util/database')
 
+const User = require('./models/user')
+
 
 const app = express();
 
@@ -20,13 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-//   User.findById(1)
-//     .then(user => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => console.log(err));
-next()
+  User.findById('65c369da8098e4f89749b0a7')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
--- models/user.js
+++ models/user.js
@@ -1,16 +1,30 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb')
+const {getDb} = require('../util/database')
+class User{
+  constructor(username,email){
+    this.username = username
+    this.email = email
+  }
 
-const sequelize = require('../util/database');
+  save(){
+    const db = getDb()
+    return db.collection('users').insertOne(this)
+    .then(()=>{
+      console.log('User created')
+    }).catch(err=>{
+      console.log(err)
+    })
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  static findById(userId){
+    const db = getDb()
+    return db.collection('users').findOne({_id: new mongodb.ObjectId(userId)})
+    .then((user)=>{
+      return user
+    }).catch(err=>{
+      console.log(err)
+    })
+  }
+}
 
 module.exports = User;
