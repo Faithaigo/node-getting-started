6008f0fecd1d3162661f8ea13b2eca6bd2a70087
faith <faith@marascientific.org>
2024-02-09 08:26:13 +0300

    setup user model

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 
 
-// const User = require('./models/user')
+const User = require('./models/user')
 
 
 const app = express();
@@ -21,14 +21,17 @@ const mongoose = require('mongoose')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('65c369da8098e4f89749b0a7')
-//     .then(user => {
-//       req.user = new User(user.username, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('65c5b6d1c84c0e600dfc08ef')
+    .then(user => {
+      /**
+       * user is a full mongoose model
+       */
+      req.user = user
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -38,6 +41,19 @@ app.use(errorController.get404);
 const dbUri = "mongodb+srv://aigofaith:WxkHZ0KA7lwk41Xf@cluster0.pkwhwvs.mongodb.net/shop?retryWrites=true&w=majority"
 
 mongoose.connect(dbUri).then(()=>{
+  User.findOne().then(user=>{
+    if(!user){
+      const user = new User({
+          name:'Faith',
+          email:'faith@gmail.com',
+          cart:{
+            items:[]
+          }
+      })
+      user.save()      
+    }
+  })
+ 
   app.listen(5000)
 }).catch(err=>{
   console.log(err)
