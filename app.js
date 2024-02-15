44d32cb58da35d80db4f53205613b7c1a259363a
faith <faith@marascientific.org>
2024-02-15 10:20:58 +0300

    Add session to user

--- app.js
+++ app.js
@@ -3,39 +3,53 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session) //pass an argument to a function
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
 
+const MONGOBD_URI = 'mongodb+srv://aigofaith:WxkHZ0KA7lwk41Xf@cluster0.pkwhwvs.mongodb.net/shop?w=majority'  
+
+const store = new MongoDBStore({
+  uri:MONGOBD_URI,
+  collection:'sessions'
+})
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+/**
+ * secret - used for assigning the #
+ * resave - The session will not be saved on every request unless something on the session changed
+ */
+app.use(session({secret: 'my secret', resave:false, saveUninitialized:false, store}))
 
-app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('65c5b6d1c84c0e600dfc08ef')
+//     .then(user => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://aigofaith:WxkHZ0KA7lwk41Xf@cluster0.pkwhwvs.mongodb.net/shop?retryWrites=true&w=majority'
-  )
+  .connect(MONGOBD_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
---
+++ controllers/auth.js
@@ -0,0 +1,35 @@
+const User = require('../models/user')
+
+exports.getLogin = (req, res, next) => {
+  // const isLoggedIn = req.get('Cookie').split(';')[2]?.trim().split('=')[1] === 'true'
+  console.log(req.session)
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: req.isLoggedIn
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  /**
+   * Max-Age is thee number in seconds how long that cookie should stay around
+   * Secure sets the cookie if the page is served via https
+   * HttpOnly- can't access the value through client side javascript, protects us from cross-site scripting
+   */
+  // res.setHeader('Set-Cookie','isLoggedIn=true; Secure')
+
+
+  User.findById('65c5b6d1c84c0e600dfc08ef')
+  .then(user => {
+    console.log(user)
+    req.session.isLoggedIn = true
+    req.session.user = user;
+    res.redirect('/')
+  })
+  .catch(err => console.log(err));
+
+  /**
+   * redirection created a brand new request
+   */
+ 
+};
